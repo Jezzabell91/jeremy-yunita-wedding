751c08f3786323a5cb06e95e5ac2a102c92d1ef7
Jeremy Bell <jeremy.bell@flightcentre.com>
2025-04-27 21:04:27 +1000

    Removed RSVP section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import HeroSection from "./HeroSection";
 import CeremonyDetails from "./CeremonyDetails";
 import AccommodationSection from "./AccommodationSection";
 import WishingWellDetails from "./WishingWellDetails";
-import RSVPSection from "./RSVPSection";
 import FAQ from "./FAQ";
 import PhotoCarousel from "./PhotoCarousel";
 
@@ -13,9 +12,8 @@ const sections = [
   { Component: CeremonyDetails, alternate: true },
   { Component: AccommodationSection, alternate: false },
   { Component: WishingWellDetails, alternate: true },
-  { Component: RSVPSection, alternate: false },
-  { Component: FAQ, alternate: true },
-  { Component: PhotoCarousel, alternate: false },
+  { Component: FAQ, alternate: false },
+  { Component: PhotoCarousel, alternate: true },
 ];
 
 export default function Home() {
